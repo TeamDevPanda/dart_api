bc80695dc70c29c83885d0d4291e8bcff1ba4676
kleak <segaud.kevin@gmail.com>
2015-04-12 18:58:05 +0200

    upload api.dart

--- bin/src/api.dart
+++ bin/src/api.dart
@@ -1,4 +1,21 @@
-part of TeamPandaApi;
+library TeamPandaApi;
+
+import 'dart:io';
+
+import 'package:rpc/rpc.dart';
+
+part 'resources/backoffices/backoffices.dart';
+part 'resources/backoffices/admins/admins.dart';
+
+part 'resources/plateformes/plateformes.dart';
+part 'resources/plateformes/settings/settings.dart';
+
+part 'resources/news/news.dart';
+
+part 'resources/users/users.dart';
+
+part 'shared/error/not_implemented.dart';
+
 
 @ApiClass(
     name: 'teampanda',
